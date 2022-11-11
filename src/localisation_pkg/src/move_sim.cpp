263aef7493b8991ccc2ff7e4316834625d992860
Jannis-Da <jannis-daiber@t-online.de>
2022-11-11 09:35:51 +0100

    Neues Package erstellt in dem Bewegung Lidar und Datenverabeitung/Algorithmus implementiert werden soll. Probleme mit Packages und CMake daher includes noch teilweise auskommentiert

---
+++ src/localisation_pkg/src/move_sim.cpp
@@ -0,0 +1,14 @@
+#include <ros/ros.h>
+//#include <gazebo/gazebo.hh>
+#include <gazebo_msgs/ModelState.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/Float32.h>
+
+
+int main(int argc, char **argv)
+{
+
+
+  return 0;
+}
